ce87902a35ed383e92408ea72a8201a54ef6105b
J.M.Bae <bejm1997@gmail.com>
2023-09-19 19:51:09 +0900

    Create MyHttpRequestManagerWithdoutMicrometer.java

---
+++ src/main/java/me/devlopery/actuatorstudy/counter/MyHttpRequestManagerWithdoutMicrometer.java
@@ -0,0 +1,19 @@
+package me.devlopery.actuatorstudy.counter;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class MyHttpRequestManagerWithdoutMicrometer {
+
+    private AtomicLong count = new AtomicLong(0);
+
+    public long getCount() {
+        return count.get() + System.currentTimeMillis();
+    }
+
+    public void increse() {
+        count.incrementAndGet();
+    }
+}
